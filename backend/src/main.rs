4e68eef6edb25d15e8d9f74663c3d0350ba9fb4e
Mqlvin <pricklydotcactus@gmail.com>
2026-04-24 20:58:48 +0100

    bind to 0.0.0.0 rather than 127.0.0.1

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -17,10 +17,10 @@ async fn main() {
         .nest("/api", get_api_router())
         .layer(cors);
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
         .unwrap();
 
-    println!("Listening on http://127.0.0.1:3000");
+    println!("Listening on http://0.0.0.0:3000");
     axum::serve(listener, app).await.unwrap();
 }
