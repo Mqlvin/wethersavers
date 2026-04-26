093f4112129af83db57989f2c31f48109c437873
Mqlvin <pricklydotcactus@gmail.com>
2026-04-26 15:10:40 +0100

    modify build script to add volume

--- buildscript.sh
+++ buildscript.sh
@@ -17,6 +17,7 @@ echo '[wethersavers] frontend files copied to stage'
 echo '[wethersavers] building main image'
 rm -f webserver.tar && podman build -t backend:glibc-bullseye -f Dockerfile && podman save localhost/backend:glibc-bullseye -o webserver.tar
 echo '[wethersavers] main image built'
+mv ./webserver.tar ../prod.tar
 echo ''
 echo '[wethersavers] to kill/remove , "docker kill wethersavers; docker container rm wethersavers"'
-echo '[wethersavers] on the server, "docker load -i webserver.tar && docker run -p 3000:3000 --name wethersavers -d --restart unless-stopped localhost/backend:glibc-bullseye"'
+echo '[wethersavers] on the server, "docker load -i prod.tar && docker run -p 3000:3000 -v wethersavers-data:/data --name wethersavers -d --restart unless-stopped localhost/backend:glibc-bullseye"'
